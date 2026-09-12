f618407d77497760040b74146e7f5d738f181e05
Srijan Agrawal <srijanagrawal284@gmail.com>
2026-09-12 11:07:26 +0530

    Add ErrorServlet to handle error responses

---
+++ src/main/java/com/srijan/servlets/ErrorServlet.java
@@ -0,0 +1,18 @@
+package com.srijan.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ErrorServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        res.setContentType("text/html");
+        PrintWriter pw = res.getWriter();
+        pw.println("<html><body><h3>An unexpected error has occurred. Please try again later.</h3></body></html>");
+    }
+}
