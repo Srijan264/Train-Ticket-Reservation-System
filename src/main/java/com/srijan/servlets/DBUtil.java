cb3b3b727498a9593a0bc3da56c1436a6f835757
Srijan Agrawal <srijanagrawal284@gmail.com>
2026-09-12 10:59:32 +0530

    Add DBUtil class for database connection management

---
+++ src/main/java/com/srijan/servlets/DBUtil.java
@@ -0,0 +1,21 @@
+package com.srijan.servlets;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBUtil {
+    private static final String URL = "jdbc:mysql://localhost:3306/train_reservation";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    public static Connection getConnection() {
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+}
