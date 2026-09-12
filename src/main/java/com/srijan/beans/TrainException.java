320ef0839b12c410366eb217d1becef7ba7fb305
Srijan Agrawal <srijanagrawal284@gmail.com>
2026-09-12 10:54:07 +0530

    Add TrainException class for custom exception handling

---
+++ src/main/java/com/srijan/beans/TrainException.java
@@ -0,0 +1,13 @@
+package com.srijan.beans;
+
+public class TrainException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public TrainException() {
+        super();
+    }
+
+    public TrainException(String message) {
+        super(message);
+    }
+}
