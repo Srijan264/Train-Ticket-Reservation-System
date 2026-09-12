4d3f1843a1c12baa86b9abfc968034971e4436cc
Srijan Agrawal <srijanagrawal284@gmail.com>
2026-09-12 11:09:32 +0530

    Implement PayServlet to handle payment requests

---
+++ src/main/java/com/srijan/servlets/PayServlet.java
@@ -0,0 +1,17 @@
+package com.srijan.servlets;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PayServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        RequestDispatcher rd = req.getRequestDispatcher("PaymentSuccess.jsp");
+        rd.forward(req, res);
+    }
+}
